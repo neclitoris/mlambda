ae3f53b54504cdd4851e29d622b137a9ad598c3c
neclitoris <34792984+neclitoris@users.noreply.github.com>
2025-07-29 20:31:48 +0300

    Use `ContT` for to interact with FFI (#2)
    * Use `ContT` for to interact with FFI
    
    * Fix inconsitency
    
    * Fix idiocy
    
    * Hide internal modules

---
+++ src/MLambda/Foreign/Utils.hs
@@ -0,0 +1,28 @@
+-- |
+-- Module      : MLambda.Matrix
+-- Description : Basic ndarray type.
+-- Copyright   : (c) neclitoris, 2025
+-- License     : BSD-3-Clause
+-- Maintainer  : nas140301@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+module MLambda.Foreign.Utils
+  ( module Numeric.Netlib.Utility
+  , asPtr
+  , asFPtr
+  ) where
+
+import Control.Monad.Cont
+import Foreign.ForeignPtr
+import Foreign.Marshal.Utils
+import Foreign.Ptr
+import Foreign.Storable
+import Numeric.Netlib.Utility
+
+
+asPtr :: Storable a => a -> ContT r IO (Ptr a)
+asPtr = ContT . with
+
+asFPtr :: ForeignPtr a -> ContT r IO (Ptr a)
+asFPtr = ContT . withForeignPtr
--- src/MLambda/Matrix.hs
+++ src/MLambda/Matrix.hs
@@ -14,15 +14,17 @@ module MLambda.Matrix
   , cross
   ) where
 
+import Control.Monad.Cont
+import Control.Monad.IO.Class
 import Data.Vector.Storable (Vector)
 import Data.Vector.Storable qualified as V
-import Foreign.C.String
 import Foreign.ForeignPtr
-import Foreign.Marshal.Utils
 import Foreign.Storable
-import GHC.IO
+import GHC.IO hiding (liftIO)
 import GHC.TypeLits
 import Numeric.BLAS.FFI.Double
+
+import MLambda.Foreign.Utils
 
 -- | 'NDArr [n1,...nd] e' is a type of arrays with dimensions @n1 x ... x nd@
 -- consisting of elements of type @e@.
@@ -34,27 +36,28 @@ deriving instance (Show e, Storable e) => Show (NDArr dim e)
 -- | Your usual matrix product. Calls into BLAS's @gemm@ operation.
 cross :: forall n m k . (KnownNat n, KnownNat m, KnownNat k)
     => NDArr [m, k] Double -> NDArr [k, n] Double -> NDArr [m, n] Double
-MkNDArr a `cross` MkNDArr b = unsafePerformIO $ do
+MkNDArr a `cross` MkNDArr b = unsafePerformIO . evalContT $ do
   let (afptr, _alen) = V.unsafeToForeignPtr0 a
       (bfptr, _blen) = V.unsafeToForeignPtr0 b
-      m = fromInteger $ fromSNat (SNat @m)
-      n = fromInteger $ fromSNat (SNat @n)
-      _k :: Int = fromInteger $ fromSNat (SNat @k)
-  cfptr <- mallocForeignPtrBytes (m * n)
+      m = fromSNat (SNat @m)
+      n = fromSNat (SNat @n)
+      k = fromSNat (SNat @k)
+      len = fromInteger $ m * n
+  cfptr <- liftIO $ mallocForeignPtrBytes len
   -- Fortran (and BLAS) uses column-major indexing,
   -- so we switch the inputs order.
-  withCString "N" \mode ->
-    withForeignPtr afptr \aptr ->
-      withForeignPtr bfptr \bptr ->
-        withForeignPtr cfptr \cptr ->
-          with (fromInteger $ fromSNat (SNat @m)) \mptr ->
-            with (fromInteger $ fromSNat (SNat @n)) \nptr ->
-              with (fromInteger $ fromSNat (SNat @k)) \kptr ->
-                with 1 \alpha ->
-                  with 0 \beta ->
-                    gemm mode mode nptr mptr kptr alpha
-                      bptr nptr
-                      aptr kptr
-                      beta cptr nptr
-  let carr = V.unsafeFromForeignPtr0 cfptr (m * n)
+  mode <- char 'N'
+  aptr <- asFPtr afptr
+  bptr <- asFPtr bfptr
+  cptr <- asFPtr cfptr
+  mptr <- asPtr $ fromInteger m
+  nptr <- asPtr $ fromInteger n
+  kptr <- asPtr $ fromInteger k
+  alpha <- asPtr 1
+  beta <- asPtr 0
+  liftIO $ gemm mode mode nptr mptr kptr alpha
+      bptr nptr
+      aptr kptr
+      beta cptr nptr
+  let carr = V.unsafeFromForeignPtr0 cfptr len
   pure $ MkNDArr carr
