6d15fd04d63659bd25d1644b1e6a1c365d92fcf6
neclitoris <34792984+neclitoris@users.noreply.github.com>
2025-07-31 16:36:13 +0300

    Implement TH for matrix definition (#3)
    Create Template Haskell quotes to safely (asserting correct dimensions from types) and efficiently (data embedded into binary) define matrices.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE OverloadedLists #-}
-
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -9,20 +9,17 @@ main :: IO ()
 main = defaultMain tests
 
 a :: NDArr [2, 3] Double
-a = MkNDArr [ 1, 2, 3
-            , 4, 5, 6
-            ]
+a = $$[mat| 1 2 3
+            4 5 6 |]
 
 b :: NDArr [3, 1] Double
-b = MkNDArr [ 1
-            , -1
-            , 0
-            ]
+b = $$[mat| 1
+            -1
+            0 |]
 
 c :: NDArr [2, 1] Double
-c = MkNDArr [ -1
-            , -1
-            ]
+c = $$[mat| -1
+            -1 |]
 
 tests :: TestTree
 tests = testGroup "Tests"
