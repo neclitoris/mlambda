3bdb2ebfd4fcd26a596737745f20bd04ed447b06
TurtlePU <sokolov.p64@gmail.com>
2025-08-03 12:52:34 +0300

    add randomness to benchmark

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,27 +1,41 @@
 {-# LANGUAGE RequiredTypeArguments #-}
 
-import Test.Tasty.Bench (bench, defaultMain, env, nf)
-
-import Data.Massiv.Array (Comp (Par), Ix2 (..), P)
-import Data.Vector.Storable (fromList)
+import Data.Massiv.Array (Comp (Par), P)
+import Data.Random.Normal (normalIO)
+import Data.Vector.Storable (replicateM, Vector)
 import GHC.TypeLits (KnownNat)
+import System.Random (mkStdGen, setStdGen)
+import Test.Tasty.Bench (bench, defaultMain, env, nf, bgroup, nfIO)
 
-import MLambda.Massiv (Mt, makeMt, mprod, value)
+import MLambda.Massiv (Mt, fromVector, mprod, value)
 import MLambda.Matrix (NDArr (MkNDArr), cross)
 
-type M = 10
-type K = 10
-type N = 10
+type M = 1000
+type K = 1000
+type N = 1000
 
-mkMt :: forall m n -> (KnownNat m, KnownNat n) => Mt P Double m n
-mkMt _ _ = makeMt Par \(i :. j) -> fromIntegral (i * j)
+setup :: IO (a -> b -> (a, b))
+setup = (,) <$ setStdGen (mkStdGen 0)
 
-mkNd :: forall m n -> (KnownNat m, KnownNat n) => NDArr [m, n] Double
-mkNd m n = MkNDArr $ fromList
-    [ fromInteger (i * j) | i <- [0..value m - 1], j <- [0..value n - 1] ]
+mkVec :: forall m n -> (KnownNat m, KnownNat n) => IO (Vector Double)
+mkVec m n = replicateM (value m * value n) normalIO
+
+mkMt :: forall m n -> (KnownNat m, KnownNat n) => IO (Mt P Double m n)
+mkMt m n = mkVec m n >>= fromVector Par
+
+mkNd :: forall m n -> (KnownNat m, KnownNat n) => IO (NDArr [m, n] Double)
+mkNd m n = MkNDArr <$> mkVec m n
 
 main :: IO ()
 main = defaultMain
-  [ env (return (mkMt M K, mkMt K N)) $ bench "Massiv matmul" . nf (uncurry mprod)
-  , env (return (mkNd M K, mkNd K N)) $ bench "OpenBLAS matmul" . nf (uncurry cross)
+  [ bgroup "random init"
+    [ bench "Massiv" $ nfIO (mkMt M N)
+    , bench "Vector" $ nfIO (mkNd M N)
+    ]
+  , bgroup "matmul"
+    [ env (setup <*> mkMt M K <*> mkMt K N) $
+      bench "Massiv" . nf (uncurry mprod)
+    , env (setup <*> mkNd M K <*> mkNd K N) $
+      bench "OpenBLAS" . nf (uncurry cross)
+    ]
   ]
--- src/MLambda/Massiv.hs
+++ src/MLambda/Massiv.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE RequiredTypeArguments #-}
 
-module MLambda.Massiv (Mt, makeMt, mprod, value) where
+module MLambda.Massiv (Mt, fromVector, mprod, value) where
 
 import Control.DeepSeq (NFData)
 import Data.Massiv.Array qualified as Massiv
 import Data.Proxy (Proxy (..))
 import GHC.TypeLits (KnownNat, natVal)
+import qualified Data.Vector.Storable as Vector
+import qualified Data.Massiv.Array.Manifest.Vector as Massiv
 
 newtype Mt r e m n = MkMt (Massiv.Matrix r e)
 
@@ -14,10 +16,15 @@ deriving newtype instance NFData (Massiv.Matrix r e) => NFData (Mt r e m n)
 value :: forall k -> (KnownNat k, Num a) => a
 value k = fromInteger $ natVal (Proxy @k)
 
-makeMt ::
-    forall m n e r. (Massiv.Load r Massiv.Ix2 e, KnownNat m, KnownNat n) =>
-    Massiv.Comp -> (Massiv.Ix2 -> e) -> Mt r e m n
-makeMt c = MkMt . Massiv.makeArray c (Massiv.Sz (value m Massiv.:. value n))
+size :: forall m n -> (KnownNat m, KnownNat n) => Massiv.Sz2
+size m n = Massiv.Sz (value m Massiv.:. value n)
+
+fromVector ::
+    forall k n e r m.
+    ( Massiv.MonadThrow m, Vector.Storable e, Massiv.Manifest r e
+    , Massiv.Load r Massiv.Ix2 e, KnownNat k, KnownNat n
+    ) => Massiv.Comp -> Vector.Vector e -> m (Mt r e k n)
+fromVector c = fmap MkMt . Massiv.fromVectorM c (size k n)
 
 mprod ::
     (Massiv.Numeric r e, Massiv.Manifest r e) =>
