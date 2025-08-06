67f8fd7d24715fcd35985e88f176127dabea1c4a
Nikita Solodovnikov <nas140301@gmail.com>
2025-08-06 23:59:48 +0300

    Clean up and add documentation

--- src/MLambda/Matrix.hs
+++ src/MLambda/Matrix.hs
@@ -15,7 +15,7 @@
 -- This module contains definition of 'NDArr' type of multidimensional arrays
 -- along with its instances and public interface.
 module MLambda.Matrix
-  ( NDArr(..)
+  ( NDArr
   , Index((:.))
   -- * Matrix operations
   , cross
@@ -50,13 +50,14 @@ import Foreign.ForeignPtr
 import Foreign.Storable
 import GHC.IO hiding (liftIO)
 import GHC.Ptr
+import GHC.TypeLits (type (<=))
 import Language.Haskell.Meta.Parse qualified as Haskell
 import Language.Haskell.TH qualified as TH
 import Language.Haskell.TH.Quote qualified as Quote
 import Numeric.BLAS.FFI.Double
 import Text.ParserCombinators.ReadP qualified as P
 
--- | `NDArr [n1,...nd] e` is a type of arrays with dimensions @n1 x ... x nd@
+-- | @NDArr [n1,...nd] e@ is a type of arrays with dimensions @n1 x ... x nd@
 -- consisting of elements of type @e@.
 newtype NDArr (dim :: [Natural]) e = MkNDArr { runNDArr :: Storable.Vector e }
   deriving (Eq, NFData)
@@ -64,7 +65,7 @@ newtype NDArr (dim :: [Natural]) e = MkNDArr { runNDArr :: Storable.Vector e }
 instance (Show e, Storable e) => Show (NDArr '[n] e) where
   show = show . runNDArr
 
-instance (KnownNat n, Enum (Index (a:r)), Bounded (Index (a:r))
+instance (KnownNat n, 1 <= n, Enum (Index (a:r)), Bounded (Index (a:r))
          , Show (NDArr (a:r) e), Storable e) => Show (NDArr (n:a:r) e) where
   showsPrec _ = (showString "[" .) . (. showString "]")
     . foldl' (.) id
@@ -94,7 +95,7 @@ crossMassiv ::
 crossMassiv a b = fromMassiv $ toMassiv a Massiv.!><! toMassiv b
 
 -- | Your usual matrix product. Calls into BLAS's @gemm@ operation.
-cross :: forall n m k . (KnownNat n, KnownNat m, KnownNat k)
+cross :: forall m k n . (KnownNat n, KnownNat m, KnownNat k)
     => NDArr [m, k] Double -> NDArr [k, n] Double -> NDArr [m, n] Double
 MkNDArr a `cross` MkNDArr b = unsafePerformIO $ evalContT do
   let (afptr, _alen) = Storable.unsafeToForeignPtr0 a
@@ -122,12 +123,27 @@ MkNDArr a `cross` MkNDArr b = unsafePerformIO $ evalContT do
 infixl 7 `cross`
 infixl 7 `crossMassiv`
 
--- | `mat` is a quasi-quote used to safely define constant matrices.
+-- | @mat@ is a quasi-quote used to safely define matrices.
 -- Usage:
--- > $$[mat| 1 2 3
--- >         4 5 6 |]
+--
+-- > [mat| 1 2 3
+-- >       4 5 6 |]
+--
 -- This quote will fail if type of the expression it appears in
 -- is not a 2x3 matrix.
+--
+-- Haskell expressions can be spliced in, too. For example,
+-- it can be used to define a 2-dimensional rotation matrix like this:
+--
+-- > rot phi = [mat| $(cos phi) $(-sin phi)
+-- >                 $(sin phi) $(cos phi)  |]
+--
+-- If the expression is just a single variable, parentheses can be omitted:
+--
+-- > [mat | $x $y
+-- >        0  $z |]
+--
+-- Whitespace inside the quote is not significant.
 mat :: Quote.QuasiQuoter
 mat = Quote.QuasiQuoter
   { quoteExp = matE
@@ -146,18 +162,19 @@ matE s = do
           case Haskell.parseExp (prev ++ str ++ ")") of
             Right e -> pure $ pure e
             Left _  -> expr (prev ++ str ++ ")")
-  let item = Right <$> P.readS_to_P (reads @Double)
+      item = Right <$> P.readS_to_P (reads @Double)
          <|> Left <$> (P.char '$' *> expr "")
-  let space = P.skipMany $ P.char ' '
-  let line = space *>P.endBy1 item space
-  let matrix = P.skipSpaces *> P.sepBy1 line (P.char '\n') <* P.skipSpaces <* P.eof
+      space = P.skipMany $ P.char ' '
+      line = space *>P.endBy1 item space
+      matrix = P.skipSpaces *> P.sepBy1 line (P.char '\n') <* P.skipSpaces <* P.eof
   l <- maybe (fail "Failed parse") (pure . fst) $ listToMaybe $ P.readP_to_S matrix s
   let m = length l
   n <- maybe (fail "Empty matrix specified") (pure . length) $ listToMaybe l
   when (n == 0) $ fail "Empty matrix specified"
   when (any ((/= n) . length) l) $ fail "Wrong column count"
   let indices = concat $ zipWith (<*>) (map ((:[]) . (+)) [0, n..]) $ map (findIndices isLeft) l
-  let exps = map (fromLeft (TH.dyn "bruh")) $ concatMap (filter isLeft) l
+  let exps = map (fromLeft (fail "You encountered a bug in the `mat` quasiquoter!"))
+        $ concatMap (filter isLeft) l
   let l' = map (fromRight 0) $ concat l
   let dat = Storable.fromList l'
       (fptr, offset, len) =
@@ -182,27 +199,33 @@ matE s = do
                 \(i, e) -> TH.noBindS [| Mutable.write mvec i $e |])
               vec <- Storable.unsafeFreeze mvec
               pure $ MkNDArr @'[$tm, $tn] @Double vec
-      |]
+         |]
 
--- | `Index dim` is the type of indices of an array of type `NDArr dim e`.
+-- | @Index dim@ is the type of indices of an array of type @`NDArr` dim e@.
+-- Instances are provided for convenient use:
+--
+-- - number literals work as one-dimensional indices of any size
+-- (if the number is outside the index range, modulo is taken, so -1 refers to the largest index)
+-- - @(:.)@ prepends a one-dimensional index to another index
+-- - @`minBound`@ represents the smallest index (first element in an n-dimensional array),
+-- @`maxBound`@ represents the largest (last element)
+-- - @`Enum`@ provides means to iterate through indices in the same
+-- order their respective elements are laid out in memory.
 data Index (dim :: [Natural]) where
-  (:.) :: Index '[n] -> Index d -> Index (n : d)
+  (:.) :: Index '[n] -> Index (d : ds) -> Index (n : d : ds)
   I :: Int -> Index '[n]
 
 deriving instance Eq (Index dim)
 deriving instance Ord (Index dim)
 deriving instance Show (Index dim)
 
-{-# COMPLETE (:.) #-}
-{-# COMPLETE I #-}
-
 infixr 5 :.
 
-instance KnownNat n => Num (Index '[n]) where
-  fromInteger = I . (`mod` natVal n) . fromInteger
+instance (KnownNat n, 1 <= n) => Num (Index '[n]) where
+  fromInteger = I . fromInteger . (`mod` natVal n)
   abs = id
   signum _ = 1
-  negate (I m) = I $ natVal n - 1 - m
+  negate (I m) = I $ (`mod` natVal n) $ natVal n - m
   (I a) + (I b) = I $ (a + b) `mod` natVal n
   (I a) * (I b) = I $ (a * b) `mod` natVal n
 
@@ -211,10 +234,10 @@ instance KnownNat n => Bounded (Index '[n]) where
   maxBound = I $ natVal n - 1
 
 instance (KnownNat n, Bounded (Index (a:r))) => Bounded (Index (n:a:r)) where
-  minBound = I 0 :. minBound
-  maxBound = I (natVal n - 1) :. maxBound
+  minBound = minBound :. minBound
+  maxBound = maxBound :. maxBound
 
-instance KnownNat n => Enum (Index '[n]) where
+instance (KnownNat n, 1 <= n) => Enum (Index '[n]) where
   fromEnum (I m) = m
   toEnum = I . (`mod` natVal n)
   succ (I m) | m == natVal n - 1 = error "Undefined succ"
@@ -222,7 +245,8 @@ instance KnownNat n => Enum (Index '[n]) where
   pred (I 0) = error "Undefined pred"
   pred (I m) = I (m - 1)
 
-instance (KnownNat n, Enum (Index (a:r)), Bounded (Index (a:r))) => Enum (Index (n:a:r)) where
+instance (KnownNat n, 1 <= n, Enum (Index (a:r)), Bounded (Index (a:r)))
+  => Enum (Index (n:a:r)) where
   fromEnum (I n :. t) = enumSize (Index (a:r)) * n + fromEnum t
   toEnum m = I q :. toEnum t
     where
@@ -236,9 +260,15 @@ instance (KnownNat n, Enum (Index (a:r)), Bounded (Index (a:r))) => Enum (Index 
 at :: (Storable e, Enum (Index dim)) => NDArr dim e -> Index dim -> e
 (MkNDArr v) `at` i = v Storable.! fromEnum i
 
--- | Construct an array from a function `f` that maps indices to elements.
--- This function is strict and therefore `f` can't refer to the result of
--- `fromFunction f`.
+infixl 9 `at`
+
+-- | Construct an array from a function @f@ that maps indices to elements.
+-- This function is strict and therefore @f@ can't refer to the result of
+-- @fromIndex f@.
+--
+-- Property:
+--
+-- prop> fromIndex f `at` i == f i
 fromIndex :: forall dim e . (Enum (Index dim), Bounded (Index dim), Storable e)
           => (Index dim -> e) -> NDArr dim e
 fromIndex f = runST do
@@ -248,9 +278,10 @@ fromIndex f = runST do
   pure $ MkNDArr vec
 
 -- | Extract a "row" from the array. If you're used to C or numpy arrays,
--- this is similar to a[i].
+-- this is similar to @a[i]@.
 row :: forall n r e {a} {b} .
     ( KnownNat n
+    , 1 <= n
     , Enum (Index r)
     , Bounded (Index r)
     , r ~ (a:b)
