1f0043c816d64b426f96a3884053c560e0272808
Nikita Solodovnikov <nas140301@gmail.com>
2025-08-17 01:49:00 +0300

    Test another property for `fromIndex`

--- test/Test/MLambda/NDArr.hs
+++ test/Test/MLambda/NDArr.hs
@@ -23,25 +23,49 @@ genDim = Gen.list (Range.between (1, 5)) genSz
 genInt :: Gen Int
 genInt = Gen.inRange $ Range.between (-1000, 1000)
 
+genIndex :: forall dim . Ix dim => Gen (Index dim)
+genIndex = case inst @dim of
+  EI -> pure E
+  Proxy @h :.= (_ :: IndexI t) -> do
+    h <- genInt
+    t <- genIndex @t
+    pure ((toEnum h :: Index '[h]) :. t)
+
+genNDArr :: forall dim . Ix dim => Gen (NDArr dim Int)
+genNDArr = do
+  Fn f <- Gen.fun genInt
+  pure $ fromIndex f
+
 instance Enum (Index dim) => Gen.Function (Index dim) where
   function gb = Gen.functionMap fromEnum toEnum <$> Gen.function gb
 
-propFromIndex :: Property ()
-propFromIndex = do
+propAtDotFromIndex :: Property ()
+propAtDotFromIndex = do
   dim <- gen genDim
   let p = withIx dim \(Proxy @dim) -> do
-            Fn f <- gen $ Gen.fun genInt
-            let is = [minBound..maxBound] :: [Index dim]
-                ndarr = fromIndex f
-                values = map f is
-                ndarrValues = map (ndarr `at`) is
-            assert $ Pred.relatedBy ("==", (or .) . zipWith (==))
-                  .$ ("fromIndex f `at` i", ndarrValues)
-                  .$ ("f i", values)
-  fromMaybe (error "propFromIndex: impossible") p
+        Fn f <- gen $ Gen.fun genInt
+        i <- gen (genIndex @dim)
+        assert $ Pred.eq
+              .$ ("at . fromIndex", (at . fromIndex) f i)
+              .$ ("id", f i)
+  fromMaybe (error "propAtDotFromIndex: impossible") p
+
+propFromIndexDotAt :: Property ()
+propFromIndexDotAt = do
+  dim <- gen genDim
+  let p = withIx dim \(Proxy @dim) ->
+        case inst @dim of
+          _ -> do
+            arr <- gen (genNDArr @dim)
+            assert $ Pred.eq
+                  .$ ("fromIndex . at", (fromIndex . at) arr)
+                  .$ ("id", arr)
+  fromMaybe (error "propFromIndexDotAt: impossible") p
 
 testFromIndex :: TestTree
-testFromIndex = testProperty "fromIndex" propFromIndex
+testFromIndex = testGroup "fromIndex"
+  [ testProperty "at . fromIndex = id" propAtDotFromIndex
+  , testProperty "fromIndex . at = id" propFromIndexDotAt]
 
 testNDArr :: TestTree
 testNDArr = testGroup "NDArr" [testFromIndex]
