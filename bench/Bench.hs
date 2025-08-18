6d3fe15833c2f244f7e768399ec72429ba853fe6
Nikita Solodovnikov <nas140301@gmail.com>
2025-08-18 03:21:27 +0300

    Benchmark initialization against `Data.Vector.Storable`

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -2,9 +2,10 @@
 
 import MLambda.Matrix
 import MLambda.NDArr
-import MLambda.TypeLits (KnownNat)
+import MLambda.TypeLits (KnownNat, natVal)
 
 import Data.Random.Normal (normalIO)
+import Data.Vector.Storable qualified as Storable
 import GHC.TypeLits (type (<=))
 import System.Random (mkStdGen, setStdGen)
 import Test.Tasty.Bench (bench, bgroup, defaultMain, env, nf, nfIO)
@@ -19,9 +20,16 @@ setup = (,) <$ setStdGen (mkStdGen 0)
 mkNd :: forall m n -> (KnownNat m, KnownNat n, 1 <= m, 1 <= n) => IO (NDArr [m, n] Double)
 mkNd m n = fromIndexM @[m, n] (const normalIO)
 
+mkVec :: forall m n -> (KnownNat m, KnownNat n)
+      => IO (Storable.Vector Double)
+mkVec m n = Storable.replicateM (natVal n * natVal m) normalIO
+
 main :: IO ()
 main = defaultMain
-  [ bench "random init" $ nfIO $ mkNd M N
+  [ bgroup "random init"
+    [ bench "NDArr" $ nfIO $ mkNd M N
+    , bench "Storable.Vector" $ nfIO $ mkVec M N
+    ]
   , env (setup <*> mkNd M K <*> mkNd K N) \input ->
     bgroup "matmul"
     [ bench "Massiv" $ nf (uncurry crossMassiv) input
